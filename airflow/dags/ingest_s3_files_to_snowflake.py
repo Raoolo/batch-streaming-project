2204482c18bb1c87f6b498dce7ea5d39d3c950d3
Raul Gatto <gatto.raulo@gmail.com>
2025-06-25 11:31:50 +0200

    Implemented read from s3 to Snowflake stage, set up local dbt

--- airflow/dags/ingest_s3_files_to_snowflake.py
+++ airflow/dags/ingest_s3_files_to_snowflake.py
@@ -1,30 +1,30 @@
 from airflow.decorators import dag, task
 from airflow.sdk import Variable
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from datetime import datetime, timedelta
 import boto3
 import logging
 import pendulum
 
 # airflow variables for S3 configuration
-s3_bucket = Variable.get("S3_BUCKET", default="ssynthetic-financial-data-batch-streaming")
-s3_prefix_raw = Variable.get("S3_PREFIX", default="rraw/")
+s3_bucket = Variable.get("S3_BUCKET", default="synthetic-financial-data-batch-streaming")
+s3_prefix_raw = Variable.get("S3_PREFIX", default="raw/")
 s3_prefix_processed = "processed/"
 s3_conn_id = "aws"
 
-# airflow variables for Snowflake configuration
-snowflake_conn_id = Variable.get("SNOWFLAKE_CONN_ID", default_var="snowflake_default")
-snowflake_database = Variable.get("SNOWFLAKE_DATABASE", default_var="MY_DATABASE")
-snowflake_schema = Variable.get("SNOWFLAKE_SCHEMA", default_var="PUBLIC")
-snowflake_stage = Variable.get("SNOWFLAKE_STAGE", default_var="MY_STAGE")
-snowflake_table = Variable.get("SNOWFLAKE_TABLE", default_var="MY_TABLE")
-snowflake_file_format = Variable.get("SNOWFLAKE_FILE_FORMAT", default_var="CSV_FORMAT")
+# airflow variables for Snowflake configuration TODO: still to set these in Airflow UI
+snowflake_conn_id = Variable.get("SNOWFLAKE_CONN_ID", default="snowflake_connection")
+snowflake_database = Variable.get("SNOWFLAKE_DATABASE", default="STREAMING_DB")
+snowflake_raw_schema = Variable.get("SNOWFLAKE_SCHEMA", default="RAW")
+snowflake_processed_schema = Variable.get("SNOWFLAKE_PROCESSED_SCHEMA", default="PROCESSED")
+snowflake_stage = Variable.get("SNOWFLAKE_STAGE", default="RAW_STAGE")
+snowflake_table = Variable.get("SNOWFLAKE_TABLE", default="TRANSACTIONS_RAW")
+snowflake_file_format = Variable.get("SNOWFLAKE_FILE_FORMAT", default="CSV_FORMAT")
 
 scheduled_interval = 5  # in minutes
 
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger("airflow_ingest_dag")
-
+logging.basicConfig(level=logging.INFO)
 
 # Define default args
 default_args = {
@@ -45,7 +45,6 @@ def ingest_s3_new_files():
 
     @task()
     def get_last_run_time(prev_start_date_success=None):
-        # i can try this prev_start_date_success 
         return prev_start_date_success    # to return the last run time
 
     @task()
@@ -76,11 +75,38 @@ def ingest_s3_new_files():
     @task()
     def process_files(file_keys: list):
         logger = logging.getLogger("airflow.task")
-        for key in file_keys:
-            logger.info(f"Processing file: {key}")
-            # TODO: Ingest logic here
+        snowflake_hook = SnowflakeHook(snowflake_conn_id=snowflake_conn_id)
+        # sf_conn = snowflake_hook.get_conn()
 
-        return file_keys
+        for key in file_keys:
+            try:
+                logger.info(f"Processing file: {key}")
+                file_path = f'@{snowflake_stage}/{key.split(s3_prefix_raw)[-1]}'
+                copy_query = f"""
+                    COPY INTO {snowflake_raw_schema}.{snowflake_table}
+                    FROM {file_path}
+                    FILE_FORMAT = (FORMAT_NAME = '{snowflake_file_format}')
+                    ON_ERROR = 'CONTINUE';
+                """
+                snowflake_hook.run(copy_query)
+                logger.info(f"Executed COPY INTO for file: {key}")
+            except Exception as e:
+                logger.error(f"Error processing file {key}: {e}")
+                continue
+
+            
+            # Move processed file
+            # s3_hook = S3Hook(aws_conn_id=s3_conn_id)
+            # source_key = key
+            # destination_key = key.replace(s3_prefix_raw, s3_prefix_processed)
+            # s3_hook.copy_object(
+            #     source_bucket_name=s3_bucket,
+            #     dest_bucket_name=s3_bucket,
+            #     source_bucket_key=source_key,
+            #     dest_bucket_key=destination_key,
+            # )
+            # s3_hook.delete_objects(bucket=s3_bucket, keys=[source_key])
+
 
     # DAG flow
     last_run = get_last_run_time()
@@ -88,4 +114,4 @@ def ingest_s3_new_files():
     process_files(new_files)
 
 
-dag_instance = ingest_s3_new_files()
+# dag_instance = ingest_s3_new_files()
--- snowflake/05_create_stage.sql
+++ snowflake/05_create_stage.sql
@@ -1,5 +1,7 @@
 USE DATABASE STREAMING_DB;
 
+-- A stage points to a location in an external storage service, such as Amazon S3
+-- and allows Snowflake to read data from or write data to that location.
 CREATE OR REPLACE STAGE RAW_STAGE
   URL = 's3://synthetic-financial-data-batch-streaming/raw/'
   STORAGE_INTEGRATION = my_s3_int
